77450cc9181cd10addb532371f4ba5522dd16547
yynlove <1993208827@qq.com>
2020-06-30 22:42:36 +0800

    新建项目

---
+++ src/main/java/com/yh/serve/ServeApplication.java
@@ -0,0 +1,14 @@
+package com.yh.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yh/serve/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.yh.serve.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+
+    @PostMapping
+    public String login(@RequestParam Map<String,Object> map){
+        System.out.println(map.toString());
+        return "200";
+    }
+
+
+}
